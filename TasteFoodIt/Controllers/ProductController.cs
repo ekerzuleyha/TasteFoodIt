2465390b8804366f635cbf5f0fc7ac02fe1712a9
zuleyhaeker.16 <zuleyhaeker.16@gmail.com>
2024-07-02 01:14:16 +0300

    .

---
+++ TasteFoodIt/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TasteFoodIt.Context;
+using TasteFoodIt.Entities;
+
+namespace TasteFoodIt.Controllers
+{
+    public class ProductController : Controller
+    {
+        TasteContext context = new TasteContext();
+
+        public ActionResult ProductList()
+        {
+            var values = context.Products.ToList();
+            return View(values);
+        }
+    }
+}
